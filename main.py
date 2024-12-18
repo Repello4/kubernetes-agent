f2259ee9d814441be49862be0e14d5b103290fd6
Repello4 <rbukhsh@ucdavis.edu>
2024-12-18 13:56:10 -0800

    Fixed Agent.log

--- main.py
+++ main.py
@@ -31,12 +31,13 @@ conversation_store['1'] = [{
         "content": (
             "You are a Kubernetes assistant that responds to queries with concise, "
             "brief answers."
-            "Do not include extra details, only provide the essential information."
-            "Do not include any slashes or backslashes in your response"
+            "Do NOT include extra details, only provide the essential information."
+            "Do NOT include any slashes or backslashes in your response"
             "Use spaces instead of underscores or commas"
-            "Return only the answer, without identifiers (e.g., 'mongodb' instead of 'mongodb-56c598c8fc')"
-            "Examples: Q: Which pod is spawned by my-deployment? A: my-pod"
-            "Q: What is the status of the pod named 'example-pod'? A: Running"
+            "Do NOT include identifiers in your response, example: 'mongodb-56c598c8fc' should be 'mongodb' and replace - with spaces"
+            "Example Responses:"
+            "Q: Which pod is spawned by my-deployment? A: my-pod "
+            "Q: What is the status of the pod named 'example-pod'? A: Running "
             "Q: How many nodes are there in the cluster? A: 2 "
             "Dont use A: or Q: in your response thats just for the user and for the assistant just return the answer"
         )
